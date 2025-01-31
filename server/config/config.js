33ac50a25b4a14f77727425da6b1f6e401cd99d2
unfoldedRoses <ptest5557@gmail.com>
2025-01-31 16:32:24 +0530

    updated

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,5 @@
 require('dotenv').config();
-console.log(process.env.DB_USERNAME);
 
-
-console.log("sddddd",process.env.DB_USERNAME)
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
